c1a81d9d64dd6962548fd9e900f4966af39c50d6
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2024-05-12 12:06:55 +0200

    fix: function

--- netlify/functions/like.mjs
+++ netlify/functions/like.mjs
@@ -4,18 +4,13 @@ import { images } from "../../src/data";
 export default async (req, context) => {
   const gallery = getStore("gallery");
 
-  let likes = JSON.parse(await gallery.get("likes"));
+  await gallery.delete("likes")
+
+  let likes = JSON.parse(await gallery.get("likes") || "{}");
   const imageId = new URL(req.url).searchParams.get("imageId");
 
   if (imageId === null) {
     return new Response("Missing imageId", { status: 400 });
-  }
-
-  if (!likes) {
-    likes = images.reduce((acc, img) => {
-      acc[img.imageId] = 0;
-      return acc;
-    }, {});
   }
 
   likes[Number(imageId)] += 1;
