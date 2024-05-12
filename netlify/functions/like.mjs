5413b03bb220f05abbcdc328e76662367c7ab9e3
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2024-05-12 12:36:58 +0200

    feat: get/post

--- netlify/functions/like.mjs
+++ netlify/functions/like.mjs
@@ -1,14 +1,25 @@
 import { getStore } from "@netlify/blobs";
 
 export default async (req, _) => {
-  const gallery = getStore("gallery");
 
-  let likes = JSON.parse(await gallery.get("likes") || "{}");
-  const imageId = new URL(req.url).searchParams.get("imageId");
-
-  if (imageId === null) {
-    return new Response("Missing imageId", { status: 400 });
+  if (req.method !== "POST") {
+    await likeImage(new URL(req.url).searchParams.get("imageId"));
   }
+
+  const likes = JSON.parse(await getStore("gallery").get("likes") || "{}");
+
+  return new Response(JSON.stringify(likes), {
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+};
+
+async function likeImage(imageId) {
+  if (imageId === null) return;
+
+  const gallery = getStore("gallery");
+  let likes = JSON.parse(await gallery.get("likes") || "{}");
 
   if (!likes[Number(imageId)]) {
     likes[Number(imageId)] = 0;
@@ -17,10 +28,4 @@ export default async (req, _) => {
   likes[Number(imageId)] += 1;
 
   await gallery.set("likes", JSON.stringify(likes));
-
-  return new Response(JSON.stringify(likes), {
-    headers: {
-      "content-type": "application/json",
-    },
-  });
-};
+}
